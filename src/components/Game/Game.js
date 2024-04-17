906cc40d2d89a8a72a7ee7fe6695722329611fb4
Michael Weber <michael.weber@spring-media.de>
2024-04-17 10:01:17 +0200

    final version

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import Form from '../Form';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import GuessHistory from '../GuessHistory/GuessHistory';
+
+import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function Game() {
-  const [guessHistory, setGuessHistory] = React.useState([]);
-
- 
+function Won({gameStatus}) {
   return (
-  <>
-    <GuessHistory guessHistory={guessHistory}></GuessHistory>
-    <Form guessHistory={guessHistory} setGuessHistory={setGuessHistory}></Form>
-  </>
+    <div className="happy banner">
+      <p>
+        <strong>Congratulations!</strong> Got it in
+        <strong>{' '}{gameStatus.count} guesses</strong>.
+      </p>
+    </div>
+  )
+}
+
+function Lost() {
+  return (<div className="sad banner">
+    <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+  </div>)
+}
+
+function Banner({ gameStatus }) {
+  return (
+    (gameStatus.status != 'playing') ? 
+    (gameStatus.status == 'won') ? <Won gameStatus={gameStatus}></Won> : <Lost></Lost> : <></>
+  );
+}
+
+function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState({ 'status': 'playing' });
+
+  function handleSubmitGuess(tentativeGuess) {
+    const nextGuesses = [...guesses, tentativeGuess];
+    setGuesses(nextGuesses);
+    if (tentativeGuess == answer) {
+      setGameStatus({ 'status': 'won', 'count' : nextGuesses.length});
+    }
+    else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus({ 'status': 'lost' });
+    }
+  }
+
+  return (
+    <>
+      <Banner gameStatus={gameStatus}></Banner>
+      <GuessResults guesses={guesses} answer={answer} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} gameStatus={gameStatus} />
+    </>
   );
 }
 
